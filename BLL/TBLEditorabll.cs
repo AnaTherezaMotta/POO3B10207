572917c277daa58789e2b51404bb8aec3d51d4b0
AnaTherezaMotta <65545245+AnaTherezaMotta@users.noreply.github.com>
2020-09-17 19:18:48 -0300

    modeloMVC

--- BLL/TBLEditorabll.cs
+++ BLL/TBLEditorabll.cs
@@ -10,29 +10,29 @@ namespace  POO3B10207 . BLL
 {
     public  class  TBLEditorabll
     {
-         DALMysql  daoBanco  privado =  novo  DALMysql ();
-        public  DataTable  listarEditora ()
+         private DALMysql daoBanco = new DALMysql();
+        public DataTable listarEditora()
         {
             string  sql  =  string . Format ( $ @ " SELECT * FROM TBL_Editora " );
-            voltar  daoBanco . executarConsulta ( sql );
+            return  daoBanco . executarConsulta ( sql );
         }
 
         public  void  criarEditora ( TBLEditoradto  dados )
         {
             string  sql  =  string . Format ( $ @ " INSERT INTO TBL_Editora VALUES (NULL, '{ dados . NomeEditora }', '{ dados . EnderecoEditora }', '{ dados . UFEditora }'); " );
-            daoBanco . executarConsulta ( sql );
+            return daoBanco . executarConsulta ( sql );
         }
 
         public  void  apagarEditora ( TBLEditoradto  dados )
         {
             string  query  =  string . Format ( $ @ " DELETE FROM TBL_Editora where id = { dados . IdEditora }; " );
-            daoBanco . executarComando ( consulta );
+            return daoBanco . executarComando ( consulta );
         }
 
         public  void  atualizarEditora ( TBLEditoradto  dados )
         {
             string  query  =  string . Format ( $ @ " UPDATE TBL_Editora SET nome = '{ dados . NomeEditora }', endereco = '{ dados . EnderecoEditora }', UF = '{ dados . UFEditora }') WHERE id = { dados . IdEditora }; " );
-            daoBanco . executarComando ( consulta );
+            return daoBanco . executarComando ( consulta );
         }
     }   
 }
