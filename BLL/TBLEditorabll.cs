f1d7cddbf71a89798dad65b9d6ef2641759fd7dd
AnaTherezaMotta <65545245+AnaTherezaMotta@users.noreply.github.com>
2020-09-17 19:10:55 -0300

    modeloMVC

--- BLL/TBLEditorabll.cs
+++ BLL/TBLEditorabll.cs
@@ -1,10 +1,10 @@
 using  System ;
-usando o  sistema . Coleções . Genérico ;
-usando o  sistema . Dados ;
-usando o  sistema . Linq ;
-usando o  sistema . Web ;
-usando  POO3B10207 . DAL ;
-usando  POO3B10207 . DTO ;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using POO3B10207.DAL;
+using POO3B10207.DTO;
 
 namespace  POO3B10207 . BLL
 {
@@ -13,25 +13,25 @@ namespace  POO3B10207 . BLL
          DALMysql  daoBanco  privado =  novo  DALMysql ();
         public  DataTable  listarEditora ()
         {
-            string  sql  =  string . Formato ( $ @ " SELECT * FROM TBL_Editora " );
+            string  sql  =  string . Format ( $ @ " SELECT * FROM TBL_Editora " );
             voltar  daoBanco . executarConsulta ( sql );
         }
 
         public  void  criarEditora ( TBLEditoradto  dados )
         {
-            string  sql  =  string . Formato ( $ @ " INSERT INTO TBL_Editora VALUES (NULL, '{ dados . NomeEditora }', '{ dados . EnderecoEditora }', '{ dados . UFEditora }'); " );
+            string  sql  =  string . Format ( $ @ " INSERT INTO TBL_Editora VALUES (NULL, '{ dados . NomeEditora }', '{ dados . EnderecoEditora }', '{ dados . UFEditora }'); " );
             daoBanco . executarConsulta ( sql );
         }
 
         public  void  apagarEditora ( TBLEditoradto  dados )
         {
-            string  query  =  string . Formato ( $ @ " DELETE FROM TBL_Editora where id = { dados . IdEditora }; " );
+            string  query  =  string . Format ( $ @ " DELETE FROM TBL_Editora where id = { dados . IdEditora }; " );
             daoBanco . executarComando ( consulta );
         }
 
         public  void  atualizarEditora ( TBLEditoradto  dados )
         {
-            string  query  =  string . Formato ( $ @ " UPDATE TBL_Editora SET nome = '{ dados . NomeEditora }', endereco = '{ dados . EnderecoEditora }', UF = '{ dados . UFEditora }') WHERE id = { dados . IdEditora }; " );
+            string  query  =  string . Format ( $ @ " UPDATE TBL_Editora SET nome = '{ dados . NomeEditora }', endereco = '{ dados . EnderecoEditora }', UF = '{ dados . UFEditora }') WHERE id = { dados . IdEditora }; " );
             daoBanco . executarComando ( consulta );
         }
     }   
